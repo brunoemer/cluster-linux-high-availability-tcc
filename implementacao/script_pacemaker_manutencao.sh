d190bb278a82daebb8fd7a15bad4cf276220060b
Bruno Emer <emerbruno@gmail.com>
2016-09-02 11:37:37 -0300

    Script manutencao pacemaker

---
+++ implementacao/script_pacemaker_manutencao.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------
+# [Bruno Emer] 2016-09-02
+#
+# Verifica e faz reinicializacao de um no do cluster pacemaker / drbd
+#
+# ------------------------------------------------------------------
+
+#verificacoes
+#se cluster esta ok
+
+
+#desativa servicos do node
+
+
+#reinicia
+
+
