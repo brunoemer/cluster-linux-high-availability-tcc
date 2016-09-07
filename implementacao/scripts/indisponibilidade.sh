4a9173a76dac207a924f2ef409d511b22282f797
Bruno Emer <emerbruno@gmail.com>
2016-09-07 20:48:20 -0300

    Continuacao capitulo 6 testes e script indisponibidade

--- implementacao/scripts/indisponibilidade.sh
+++ implementacao/scripts/indisponibilidade.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Copyright (c) 2008 Wanderson S. Reis < wasare@gmail .com >
+#!/bin/bash
+# Copyright (c) 2008 Wanderson S. Reis <wasare@gmail.com>
 #
 # Adaptado para gerar estatisticas do ping.
 # Bruno Emer <emerbruno@gmail.com> 2016
@@ -10,8 +10,8 @@
 #
 IP=$1
 
-ping $1 > $1-stat.log &
-FOO_PID=$!
+ping $1 > $1-stat-$2.log &
+PINGPID=$!
 
 RETORNO=0
 while [ $RETORNO -eq 0 ]
@@ -27,9 +27,10 @@ do
 done
 FINAL=`date +%s`
 INTERVALO=$(( $FINAL - $INICIAL ))
-echo "host $1 : $INTERVALO s" > $1-downtime.log
+echo "host $1 : $INTERVALO s" > $1-downtime-$2.log
 
-pkill -SIGINT ping
+#pkill -SIGINT ping
+kill -SIGINT $PINGPID
 
 exit 0
 
